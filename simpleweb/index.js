c405e3803d6f76e0250c41852de6ec90e995c452
mustafa-coding <mustafa89.mca@gmail.com>
2019-07-13 20:08:27 +0000

    Added simpleweb and Visits projects

---
+++ simpleweb/index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+ 
+const app = express();
+ 
+app.get('/', (req, res) => {
+  res.send('How are you doing?');
+});
+ 
+app.listen(5000, () => {
+  console.log('Listening on port 5000');
+});
